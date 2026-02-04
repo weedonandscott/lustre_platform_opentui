47a073cf611d3b550bc770d2b039521f08ded20a
Ofek Doitch <Tech@WeedonAndScott.com>
2026-02-04 13:15:43 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix-flake-based development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Editors
+            nodePackages.typescript-language-server
+            taplo
+            typescript
+
+            # FE
+            node2nix
+            nodejs_24
+            bun
+
+            gleam
+            beam28Packages.erlang
+            beam28Packages.rebar3
+          ];
+        };
+      });
+    };
+}
